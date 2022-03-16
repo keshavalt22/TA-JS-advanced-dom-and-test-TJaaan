b6cd39053f73eadbc34ac0211b6ef99419a0e706
keshavalt22 <keshavkhatri35@gmail.com>
2022-03-16 22:55:32 +0530

    assignemnt

--- block-BJaair/code/script.js
+++ block-BJaair/code/script.js
@@ -1,12 +1,5 @@
 let root = document.querySelector("ul");
 
 function addQuotes(){
-        let li = document.createElement("li");
-        let input = document.createElement("input");
-        let input = document.createElement("input");
-        input.innerText = quotes[index].quoteText;
-        cite.innerText = quotes[index].quoteAuthor;
-        li.append(blockquote,cite); 
-
-        root.append(li);
+    
 }
