44f975754bedf85392604e488224c82ab8827db5
keshavalt22 <keshavkhatri35@gmail.com>
2022-03-17 21:44:45 +0530

    assignemnt

--- block-BJaair/code/script.js
+++ block-BJaair/code/script.js
@@ -1,5 +1,30 @@
-let root = document.querySelector("ul");
+let form = document.querySelector("form");
+let ul = document.querySelector("ul");
 
-function addQuotes(){
-    
+let cardsData = JSON.parse(localStorage.getItem("cards"));
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let title = event.target.elements.title.value;
+    let category = event.target.elements.category.value;
+
+    cardsData.push({title, category});
+    localStorage.setItem("cards", JSON.stringify(cardsData));
+    createUI(cardsData, ul);
+})
+
+function createUI(data, root){
+    let fragment = new DocumentFragment;
+   data.forEach((cardInfo) => {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    p.innerText = cardInfo.title;
+    let h2 = document.createElement("h2");
+    h2.innerText = cardInfo.category;
+    li.append(p,h2);
+    fragment.appendChild(li);
+   })
+   root.append(fragment);
 }
